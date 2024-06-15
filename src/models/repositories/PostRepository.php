81abf34b665e494b1d7434f8acbebf45505054e0
Hazardben <mindshunter071@gmail.com>
2024-06-15 20:46:29 +0200

    edit

--- src/models/repositories/PostRepository.php
+++ src/models/repositories/PostRepository.php
@@ -1,38 +1,34 @@
 <?php
-
-abstract class PostRepository extends Db{
-    public static function request($request){
-        // Connect to the database and execute the query
+abstract class PostRepository extends Db
+{
+    private static function request($request)
+    {
         $result = self::getInstance()->query($request);
-        // Disconnect from the database
-        self::disconnect();
-        // Return the result of the query
         return $result;
     }
 
-    public static function getPosts(){
-        return self::request("SELECT * FROM post")->fetchAll(PDO::FETCH_ASSOC);
+    public static function getPosts()
+    {
+        return self::request("SELECT * FROM Post ")->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public static function getPostById($id){
-        return self::request("SELECT * FROM post WHERE id=$id")->fetch(PDO::FETCH_ASSOC);
+    public static function getPostByid($id)
+    {
+        return self::request("SELECT * FROM Post WHERE id=".$id)->fetch(PDO::FETCH_ASSOC);
     }
 
-    public static function getPostByUser($authorId){
-        return self::request("SELECT * FROM post WHERE athor_id='$authorId'")->fetch(PDO::FETCH_ASSOC);
-    }
 
-    public static function insertIntoDb(Post $post){
-     
-
-        return self::request("INSERT INTO post (title , content,athor_id) VALUES(
-            '" . $post->getTitle() . "' ,
-        '" . $post->getContent() . "', 
-        '" . $post->getAuthorId(). "')");
+    public static function insertIntoDb(Post $post)
+    {
+        return self::request("INSERT INTO post (title, message, author) VALUES ('" . $post->getTitle() . "', '" . $post->getMessage() . "', '" . $post->getAuthor() . "')");
     }
-public static function deletPost($id){
-    return self::request("DELETE FROM post where id = $id");
+    public static function updatePost(Post $post)
+    {
+        return self::request("UPDATE post SET title ='" . $post->getTitle() . "', message='" . $post->getMessage() . "' WHERE id=".$post->getId());
+    }
+    public static function deletePost(Post $post)
+    {
+        return self::request("DELETE FROM Post WHERE id =" . $post->getId());
+    }
 }
-
- 
-} 
+?>
--- src/models/repositories/UserRepository.php
+++ src/models/repositories/UserRepository.php
@@ -1,35 +1,23 @@
 <?php
-
-abstract class userRepository extends Db{
-    public static function request($request){
-        // Connect to the database and execute the query
-        $result = self::getInstance()->query($request);
-        // Disconnect from the database
-        self::disconnect();
-        // Return the result of the query
-        return $result;
+abstract class UserRepository extends Db
+{
+    private static function request($request)
+    {
+        return self::getInstance()->query($request);
+    }
+    public static function getUserByMail($mail)
+    {
+        return self::request("SELECT * FROM user WHERE mail= '$mail'")->fetch(PDO::FETCH_ASSOC);
     }
 
-    public static function getUsers(){
-        return self::request("SELECT * FROM users")->fetchAll(PDO::FETCH_ASSOC);
+    public static function getUserById($id)
+    {
+        return self::request("SELECT * FROM user WHERE id= $id")->fetch(PDO::FETCH_ASSOC);
     }
 
-    public static function getUserById($id){
-        return self::request("SELECT * FROM users WHERE id=$id")->fetch(PDO::FETCH_ASSOC);
+    public static function insertIntoDb(User $user)
+    {
+        return self::request("INSERT INTO user(name, firstname, password, mail) VALUES ('" . $user->getName() . "', '" . $user->getFirstname() . "', '" . $user->getPassword() . "', '" . $user->getMail() . "')");
     }
-
-    public static function getUserByEmail($email){
-        return self::request("SELECT * FROM users WHERE email='$email'")->fetch(PDO::FETCH_ASSOC);
-    }
-
-    public static function insertIntoDb(User $user){
-     
-
-        return self::request("INSERT INTO users(name, firstName, email, password) VALUES('" . $user->getName() . "' ,
-        '" . $user->getfirstName() . "', 
-        '" . $user->getEmail() . "',
-        '" . $user->getPassword() . "')");
-    }
-
 }
-   
+?>
